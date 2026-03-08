fcfb80750b30f0956dd0870a7c656902a3576942
Lynn Novaes <142624913+nvsslynn@users.noreply.github.com>
2026-03-07 23:23:37 -0300

    Arquivos da Raiz

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask, url_for, render_template, jsonify
+from datetime import datetime as dt
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html", hour=22, min=31)
+
+@app.route("/raw")
+def rawpage():
+    now = dt.now()
+    return jsonify({
+        "hour": now.strftime("%H"),
+        "mins": now.strftime("%M"),
+        "secs": now.strftime("%S")
+    })
+
+app.run(debug=True)
